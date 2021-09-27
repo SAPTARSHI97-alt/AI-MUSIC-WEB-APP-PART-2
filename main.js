84a47787b236a58420c323a64de3b27230a447c2
SAPTARSHI97-alt <82492377+SAPTARSHI97-alt@users.noreply.github.com>
2021-09-27 17:51:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song = "";
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function preload(){
+    song = loadSound("baaren_gates_devil.mp3");
+}
+
+function play(){
+song.play();
+}
